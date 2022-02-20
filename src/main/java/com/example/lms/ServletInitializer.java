61ec89a3ac0e20e6e435cce356775043537d70f9
owrx887 <owrx887@gmail.com>
2022-02-20 22:05:36 +0900

    lms 초기화 설정

--- src/main/java/com/example/lms/ServletInitializer.java
+++ src/main/java/com/example/lms/ServletInitializer.java
@@ -4,6 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
+    //2022.02.20
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
