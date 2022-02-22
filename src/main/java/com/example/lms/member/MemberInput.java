c533b005fb35689f212b96e2642346ad42bcbdaf
owrx887 <owrx887@gmail.com>
2022-02-22 23:01:06 +0900

    lms 초기화 설정

---
+++ src/main/java/com/example/lms/member/MemberInput.java
@@ -0,0 +1,14 @@
+package com.example.lms.member;
+
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+public class MemberInput {
+    private String userId;
+    private String userName;
+    private String password;
+    private String phone;
+
+}
