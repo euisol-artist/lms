7bec132f830eff1a845e9ab91a569e7bf681712e
owrx887 <owrx887@gmail.com>
2022-02-25 22:07:30 +0900

    lms 초기화 설정

--- src/main/java/com/example/lms/components/MailComponents.java
+++ src/main/java/com/example/lms/components/MailComponents.java
@@ -3,7 +3,11 @@ package com.example.lms.components;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+
+import javax.mail.internet.MimeMessage;
 
 @RequiredArgsConstructor
 @Component
@@ -18,4 +22,28 @@ public class MailComponents {
 
         javaMailSender.send(msg);
     }
+
+    public boolean sendMail(String mail, String subject, String text) {
+
+        boolean result = false;
+
+        MimeMessagePreparator msg = new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                mimeMessageHelper.setTo(mail);
+                mimeMessageHelper.setSubject(subject);
+                mimeMessageHelper.setText(text, true);
+            }
+        };
+        try {
+            javaMailSender.send(msg);
+            result = true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result;
+    }
+
 }
